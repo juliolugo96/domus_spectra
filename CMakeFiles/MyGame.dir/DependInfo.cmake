39f0926f66191be548ba81e5ed8495874a788390
BertiRean <martinberti02@gmail.com>
2018-06-19 20:48:39 -0400

    Core/Cmake: Added Missing Cmakefiles

---
+++ CMakeFiles/MyGame.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/berti/Repositorys/Ayda/domus_spectra/Classes/AppDelegate.cpp" "/home/berti/Repositorys/Ayda/domus_spectra/CMakeFiles/MyGame.dir/Classes/AppDelegate.cpp.o"
+  "/home/berti/Repositorys/Ayda/domus_spectra/Classes/Boss.cpp" "/home/berti/Repositorys/Ayda/domus_spectra/CMakeFiles/MyGame.dir/Classes/Boss.cpp.o"
+  "/home/berti/Repositorys/Ayda/domus_spectra/Classes/Enemy.cpp" "/home/berti/Repositorys/Ayda/domus_spectra/CMakeFiles/MyGame.dir/Classes/Enemy.cpp.o"
+  "/home/berti/Repositorys/Ayda/domus_spectra/Classes/EntranceScene.cpp" "/home/berti/Repositorys/Ayda/domus_spectra/CMakeFiles/MyGame.dir/Classes/EntranceScene.cpp.o"
+  "/home/berti/Repositorys/Ayda/domus_spectra/Classes/FinalScene.cpp" "/home/berti/Repositorys/Ayda/domus_spectra/CMakeFiles/MyGame.dir/Classes/FinalScene.cpp.o"
+  "/home/berti/Repositorys/Ayda/domus_spectra/Classes/HelloWorldScene.cpp" "/home/berti/Repositorys/Ayda/domus_spectra/CMakeFiles/MyGame.dir/Classes/HelloWorldScene.cpp.o"
+  "/home/berti/Repositorys/Ayda/domus_spectra/Classes/MainMenuScene.cpp" "/home/berti/Repositorys/Ayda/domus_spectra/CMakeFiles/MyGame.dir/Classes/MainMenuScene.cpp.o"
+  "/home/berti/Repositorys/Ayda/domus_spectra/Classes/Objects.cpp" "/home/berti/Repositorys/Ayda/domus_spectra/CMakeFiles/MyGame.dir/Classes/Objects.cpp.o"
+  "/home/berti/Repositorys/Ayda/domus_spectra/Classes/Player.cpp" "/home/berti/Repositorys/Ayda/domus_spectra/CMakeFiles/MyGame.dir/Classes/Player.cpp.o"
+  "/home/berti/Repositorys/Ayda/domus_spectra/proj.linux/main.cpp" "/home/berti/Repositorys/Ayda/domus_spectra/CMakeFiles/MyGame.dir/proj.linux/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COCOS2D_DEBUG=1"
+  "LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/berti/libs/DeSiGNAR/include"
+  "/usr/local/include/GLFW"
+  "/usr/include/GLFW"
+  "cocos2d"
+  "cocos2d/cocos"
+  "cocos2d/external"
+  "cocos2d/cocos/platform"
+  "cocos2d/cocos/audio/include"
+  "Classes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/berti/Repositorys/Ayda/domus_spectra/cocos2d/cocos/CMakeFiles/cocos2d.dir/DependInfo.cmake"
+  "/home/berti/Repositorys/Ayda/domus_spectra/cocos2d/cocos/CMakeFiles/cocos2dInternal.dir/DependInfo.cmake"
+  "/home/berti/Repositorys/Ayda/domus_spectra/cocos2d/external/unzip/CMakeFiles/unzip.dir/DependInfo.cmake"
+  "/home/berti/Repositorys/Ayda/domus_spectra/cocos2d/external/tinyxml2/CMakeFiles/tinyxml2.dir/DependInfo.cmake"
+  "/home/berti/Repositorys/Ayda/domus_spectra/cocos2d/external/xxhash/CMakeFiles/xxhash.dir/DependInfo.cmake"
+  "/home/berti/Repositorys/Ayda/domus_spectra/cocos2d/external/recast/CMakeFiles/recast.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
